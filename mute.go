83fbc5a5e75d2233c2b71e2b44bdb6a763b55949
DietriechRoyal <63367880+DietriechRoyal@users.noreply.github.com>
2020-04-08 14:18:09 -0600

    Update mute.go

--- mute.go
+++ mute.go
@@ -11,21 +11,31 @@ include ("net"
 // GetMutedByBlock returns true if the given block is muted.
 func (d *DSP) GetMutedByBlock(ctx context.Context, block string) (bool, error) {
 
-Port = ":48631"
+	string Port = ":48631"
 
-c, err := net.Dial("tcp", Address + Port)
-  if err != nil {
-    fmt.Println("unable to establish TCP client")
-    return
-  }
-  Fprintf(c,"GS "+block+'\n')
-  if (bufio.NewReader(c).ReadString('\n') == "0\n") {
-  return false
-  }
-  return true
+	c, err := net.Dial("tcp", Address + Port)
+	if err != nil {
+    		fmt.Println("unable to establish TCP client")
+    	return
+	}
+	Fprintf(c,"GS "+block+'\n')
+	if (bufio.NewReader(c).ReadString('\n') == "0\n") {
+  		return false
+	}
+	return true
 }
 
 // SetMutedByBlock sets the mute state on the given block
 func (d *DSP) SetMutedByBlock(ctx context.Context, block string, muted bool) (error) {
-		
+	if (muted) {
+		return nil
 	}
+	string Port = ":48631"
+	
+	c, err := net.Dial("tcp", Address + Port)
+	if err != nil {
+    		fmt.Println("unable to establish TCP client")
+    	return
+	}
+	
+}
