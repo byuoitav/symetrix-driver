e72d9e2e383b3191207c3d20c4060b32f05b94ce
DietriechRoyal <63367880+DietriechRoyal@users.noreply.github.com>
2020-04-15 16:37:01 -0600

    Update volume.go

--- volume.go
+++ volume.go
@@ -1,12 +1,9 @@
 package symetrix
 
 import (
-	"bufio"
 	"context"
 	"fmt"
-	"math"
 	"net"
-	"strconv"
 )
 
 const (
@@ -14,56 +11,79 @@ const (
 )
 
 // GetVolumeByBlock returns the volume [0, 100] of the given block.
-func (d *SymetrixDSP) GetVolumeByBlock(ctx context.Context, block string) (int, error) {
+func (d *DSP) GetVolumeByBlock(ctx context.Context, block string) (int, error) {
 
-	c, err := net.Dial("tcp", d.Address+":48631")
+	s, err := net.ResolveUDPAddr("udp4", address+":48631")
+	c, err := net.DialUDP("udp4", nil, s)
 	if err != nil {
-		fmt.Println("unable to establish TCP client")
-		return -1, fmt.Errorf("unable to establish TCP client: %w", err)
+		return -1, fmt.Errorf("unable to establish UDP client: %w", err)
 	}
-	fmt.Fprintf(c, "GS %v\n", block)
-	strResult, err := bufio.NewReader(c).ReadString('\n')
+
+    defer c.Close()
+
+	text := fmt.Sprintf("GS %v\r\n", block)
+	data := []byte(text)
+	_, err = c.Write(data)
+
 	if err != nil {
+		fmt.Println(err)
+		return -1, fmt.Errorf("unable to write to client: %w", err)
+	}
+
+	buffer := make([]byte, 1024)
+	n, _, err := c.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println(err)
 		return -1, fmt.Errorf("unable to read response: %w", err)
 	}
-	result, err := strconv.Atoi(strResult)
-	if err != nil {
-		return -1, fmt.Errorf("unable to convert volume string to int: %w", err)
-	}
+
+	val := string(buffer[0:n])
+	result, err := strconv.ParseInt(strings.TrimSpace(val), 10, 64)
+	
 	if result > maxVolumeLevel {
-		fmt.Println("fader is above max volume level")
-		err = d.SetVolumeByBlock(ctx, block, 100)
-		if err != nil {
-			return -1, fmt.Errorf("unable to set volume: %w", err)
-		}
 		result = 56000
 	}
-	result = int(math.Round((float64(result) / maxVolumeLevel) * 100))
 
-	return result, nil
+	volume := int(float64(result*100.0) / maxVolumeLevel)
+
+	return volume, nil
 }
 
 // SetVolumeByBlock sets the volume on the given block. Volume must be in the range [0, 100].
-func (d *SymetrixDSP) SetVolumeByBlock(ctx context.Context, block string, volume int) error {
+func (d *DSP) SetVolumeByBlock(ctx context.Context, block string, volume int) error {
 	if volume < 0 || volume > 100 {
 		return fmt.Errorf("volume must be in range [0, 100]")
 	}
-
-	volume *= maxVolumeLevel
-	c, err := net.Dial("tcp", d.Address+":48631")
+	
+	s, err := net.ResolveUDPAddr("udp4", address+":48631")
+	c, err := net.DialUDP("udp4", nil, s)
 	if err != nil {
-		fmt.Println("unable to establish TCP client")
-		return fmt.Errorf("unable to establish TCP client: %w", err)
+		return fmt.Errorf("unable to establish UDP client: %w", err)
 	}
 
-	fmt.Fprintf(c, "CS %v %v\n", block, volume)
-	result, err := bufio.NewReader(c).ReadString('\n')
+    defer c.Close()
+    volume = volume * (maxVolumeLevel / 100)
+	text := fmt.Sprintf("CS %v %v\r\n", block, volume)
+	data := []byte(text)
+	_, err = c.Write(data)
+
 	if err != nil {
+		fmt.Println(err)
+		return fmt.Errorf("unable to write to client: %w", err)
+	}
+
+	buffer := make([]byte, 1024)
+	n, _, err := c.ReadFromUDP(buffer)
+	if err != nil {
+		fmt.Println(err)
 		return fmt.Errorf("unable to read response: %w", err)
 	}
-	if result != fmt.Sprintf("ACK\n#0000%v=%v\n", block, volume) {
-		return fmt.Errorf("Unsuccessful")
-	} else {
-		return nil
-	}
+    val := fmt.Sprintf("%s", string(buffer[0:n]))
+
+    if (val == "ACK\r") {
+        fmt.Println("GOOD\n")
+        return nil
+    }
+
+	return fmt.Errorf("Unsuccessful Volume Change")
 }
