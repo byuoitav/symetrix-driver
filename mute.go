6601b486bc77c578040bdcd4d10516df801913b6
Andrew Werner <klinc9@gmail.com>
2020-10-02 14:27:46 -0600

    updating functions to match new API interfaces

--- mute.go
+++ mute.go
@@ -14,43 +14,49 @@ const (
 )
 
 // GetMutedByBlock returns true if the given block is muted.
-func (d *DSP) GetMutedByBlock(ctx context.Context, block string) (bool, error) {
-	s, err := net.ResolveUDPAddr("udp4", d.Address+":48631")
-	c, err := net.DialUDP("udp4", nil, s)
-	if err != nil {
-		return false, fmt.Errorf("unable to establish UDP client: %w", err)
+func (d *DSP) GetMutes(ctx context.Context, blocks []string) (map[string]bool, error) {
+	toReturn := make(map[string]bool)
+
+	for _, block := range blocks {
+		s, err := net.ResolveUDPAddr("udp4", d.Address+":48631")
+		c, err := net.DialUDP("udp4", nil, s)
+		if err != nil {
+			return map[string]bool{block: false}, fmt.Errorf("unable to establish UDP client: %w", err)
+		}
+
+		defer c.Close()
+		c.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+		text := fmt.Sprintf("GS %v\r\n", block)
+		data := []byte(text)
+		_, err = c.Write(data)
+		if err != nil {
+			fmt.Println(err)
+			return map[string]bool{block: false}, fmt.Errorf("unable to write to client: %w", err)
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := c.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return map[string]bool{block: false}, fmt.Errorf("unable to read response: %w", err)
+		}
+
+		val := string(buffer[0:n])
+		result, err := strconv.ParseInt(strings.TrimSpace(val), 10, 64)
+
+		if result == MUTE_ENABLE_VAL {
+			toReturn[block] = true
+		} else {
+			toReturn[block] = false
+		}
 	}
 
-	defer c.Close()
-	c.SetReadDeadline(time.Now().Add(5 * time.Second))
-
-	text := fmt.Sprintf("GS %v\r\n", block)
-	data := []byte(text)
-	_, err = c.Write(data)
-	if err != nil {
-		fmt.Println(err)
-		return false, fmt.Errorf("unable to write to client: %w", err)
-	}
-
-	buffer := make([]byte, 1024)
-	n, _, err := c.ReadFromUDP(buffer)
-	if err != nil {
-		fmt.Println(err)
-		return false, fmt.Errorf("unable to read response: %w", err)
-	}
-
-	val := string(buffer[0:n])
-	result, err := strconv.ParseInt(strings.TrimSpace(val), 10, 64)
-
-	if result == MUTE_ENABLE_VAL {
-		return true, nil
-	}
-
-	return false, nil
+	return toReturn, nil
 }
 
 // SetMutedByBlock sets the mute state on the given block
-func (d *DSP) SetMutedByBlock(ctx context.Context, block string, muted bool) error {
+func (d *DSP) SetMute(ctx context.Context, block string, muted bool) error {
 
 	s, err := net.ResolveUDPAddr("udp4", d.Address+":48631")
 	c, err := net.DialUDP("udp4", nil, s)
--- volume.go
+++ volume.go
@@ -14,47 +14,51 @@ const (
 )
 
 // GetVolumeByBlock returns the volume [0, 100] of the given block.
-func (d *DSP) GetVolumeByBlock(ctx context.Context, block string) (int, error) {
+func (d *DSP) GetVolumes(ctx context.Context, blocks []string) (map[string]int, error) {
+	toReturn := make(map[string]int)
 
-	s, err := net.ResolveUDPAddr("udp4", d.Address+":48631")
-	c, err := net.DialUDP("udp4", nil, s)
-	if err != nil {
-		return -1, fmt.Errorf("unable to establish UDP client: %w", err)
+	for _, block := range blocks {
+		s, err := net.ResolveUDPAddr("udp4", d.Address+":48631")
+		c, err := net.DialUDP("udp4", nil, s)
+		if err != nil {
+			return map[string]int{block: -1}, fmt.Errorf("unable to establish UDP client: %w", err)
+		}
+
+		defer c.Close()
+		c.SetReadDeadline(time.Now().Add(5 * time.Second))
+
+		text := fmt.Sprintf("GS %v\r\n", block)
+		data := []byte(text)
+		_, err = c.Write(data)
+
+		if err != nil {
+			fmt.Println(err)
+			return map[string]int{block: -1}, fmt.Errorf("unable to write to client: %w", err)
+		}
+
+		buffer := make([]byte, 1024)
+		n, _, err := c.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println(err)
+			return map[string]int{block: -1}, fmt.Errorf("unable to read response: %w", err)
+		}
+
+		val := string(buffer[0:n])
+		result, err := strconv.ParseInt(strings.TrimSpace(val), 10, 64)
+
+		if result > maxVolumeLevel {
+			result = 56000
+		}
+
+		volume := int(float64(result*100.0) / maxVolumeLevel)
+		toReturn[block] = volume
 	}
 
-	defer c.Close()
-	c.SetReadDeadline(time.Now().Add(5 * time.Second))
-
-	text := fmt.Sprintf("GS %v\r\n", block)
-	data := []byte(text)
-	_, err = c.Write(data)
-
-	if err != nil {
-		fmt.Println(err)
-		return -1, fmt.Errorf("unable to write to client: %w", err)
-	}
-
-	buffer := make([]byte, 1024)
-	n, _, err := c.ReadFromUDP(buffer)
-	if err != nil {
-		fmt.Println(err)
-		return -1, fmt.Errorf("unable to read response: %w", err)
-	}
-
-	val := string(buffer[0:n])
-	result, err := strconv.ParseInt(strings.TrimSpace(val), 10, 64)
-
-	if result > maxVolumeLevel {
-		result = 56000
-	}
-
-	volume := int(float64(result*100.0) / maxVolumeLevel)
-
-	return volume, nil
+	return toReturn, nil
 }
 
 // SetVolumeByBlock sets the volume on the given block. Volume must be in the range [0, 100].
-func (d *DSP) SetVolumeByBlock(ctx context.Context, block string, volume int) error {
+func (d *DSP) SetVolume(ctx context.Context, block string, volume int) error {
 	if volume < 0 || volume > 100 {
 		return fmt.Errorf("volume must be in range [0, 100]")
 	}
